4ed98407a1632a0f0dfb279c7600efeb7fc6526f
Игорь Некрашевич <73841739+IgorNekradhevich@users.noreply.github.com>
2020-12-07 21:21:41 +0700

    07.12

---
+++ MyChatServer/ChatServer/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    public class Client
+    {
+        public string _name { get; set; }
+        public int _id {get;set; }
+        public TcpClient _client { get; }
+        public Client(string name, int id, TcpClient client)
+        {
+            _name = name;
+            _id = id;
+            _client = client;
+        }
+
+    }
+}
---
+++ MyChatServer/ChatServer/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    class Program
+    {
+       static  List<Client> clients;
+       static int count = 0;
+        static void Main(string[] args)
+        {
+            clients = new List<Client>();
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 8888);
+            tcpListener.Start();
+
+            while (true)
+            {
+                clients.Add(new Client("user", count, tcpListener.AcceptTcpClient()));
+                count++;
+                Console.WriteLine("Новый пользователь");
+                Task.Factory.StartNew(() => { Listner(clients[clients.Count - 1]); });
+            }
+        }
+
+        public static string GetOnlines()
+        {
+            string text = "<Onlines>|";
+            clients.ForEach(el => text += (el._name + "|"));
+            return text += "<end>";  
+        }
+        public static void SendToAll(string smt)
+        {
+            byte[] buffer = new byte[255];
+            buffer = Encoding.UTF8.GetBytes(smt);
+
+            foreach(Client SendTo in clients)
+            {
+                NetworkStream stream = SendTo._client.GetStream();
+                stream.Write(buffer, 0, buffer.Length);
+            }
+
+           // clients.ForEach(el => el._client.GetStream()
+           //.Write(buffer, 0, buffer.Length));
+        }
+
+       static  void Listner (Client client)
+        {
+
+            bool isOnline = true;
+            while(isOnline)
+            {
+               
+                    NetworkStream stream = client._client.GetStream();
+                    byte[] buffer = new byte[255];
+
+                try
+                {
+                    stream.Read(buffer, 0, 255);
+                    string something = Encoding.UTF8.GetString(buffer);
+
+                    if (something.Contains("<MyName>"))
+                    {
+                        client._name = something.Remove(0, 8);
+                        client._name = client._name.Remove(client._name.IndexOf('\0'));
+                        SendToAll(GetOnlines());
+                    }
+                    else
+                    {
+                        string message = client._name + ":" + something;
+                        Console.WriteLine((Encoding.UTF8.GetString(buffer)));
+                        if (buffer.Length > 0)
+                        {
+                            SendToAll(message);
+                            //  Console.WriteLine(message);
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Кто то нас покинул");
+                    clients.Remove(client);
+                    SendToAll(GetOnlines());
+                    isOnline = false;
+                }
+            }
+       
+        }
+    }
+}
